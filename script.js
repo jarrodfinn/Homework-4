a7e45c062a1a01dc7fc40cf2af2f43bbb4551a76
Jarrod Finn <jarrod.p.finn@gmail.com>
2019-12-11 10:13:31 -0500

    basic html completed

---
+++ script.js
@@ -0,0 +1,59 @@
+// Input Box
+// Event Listener on Input Box
+// Submit button
+// Event Listener on button
+// Form to hold question
+// Form to chooose multiple choice answers
+// Event Listener
+
+// Variables:
+var quizContainer = document.getElementById('quiz');
+var startBtn = document.getElementById('#start-button');
+var quizStart = document.getElementById("#show-quiz");
+var set
+
+// add EventListener to "startBtn" to call the startQuiz function
+document.getElementById("start-button").addEventListener("click", startQuiz)
+
+
+
+
+
+
+function startQuiz() {
+	// runs for loop through questions
+	for (var i = 0; i < questions.length; i = i + 1);
+}
+
+// How do you deliver this? Here are some guidelines:
+
+
+// Play proceeds as follows:
+
+
+// The user arrives at the landing page and is presented with a call-to-action to "Start Quiz." Also note the navigation option to "View Highscores" and the "Time" value set at 0.
+
+
+// Clicking the "Start Quiz" button presents the user with a series of questions. The timer is initialized with a value and immediately begins countdown.
+
+
+// Score is calculated by time remaining. Answering quickly and correctly results in a higher score. Answering incorrectly results in a time penalty (for example, 15 seconds are subtracted from time remaining).
+
+
+// When time runs out and/or all questions are answered, the user is presented with their final score and asked to enter their initials. Their final score and initials are then stored in localStorage.
+
+
+
+
+// Your application should also be responsive, ensuring that it adapts to multiple screen sizes.
+
+
+// Refer to the animated GIF below for a demonstration of the application functionality.
+
+
+
+
+
+
+
+
